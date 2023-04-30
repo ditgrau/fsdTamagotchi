460294026c4500f7b3a9aecce77e5388d2ece7f9
DitGrau <juditgraup@gmail.com>
2023-05-01 01:25:11 +0200

    Acabado el javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 
-function birth(){
-    let tamagotchi = document.getElementById("content");
-    tamagotchi.classList.add("hatching");
 
+function birth() {
+    let displayImage = document.getElementById('content')
+    if (displayImage.src.match('./img/huevo.PNG')) {
+        displayImage.src = "./img/tama.PNG"
+    }
+}
+
+function popo() {
+    let displayImage = document.getElementById('content')
+    if (displayImage.src.match('./img/tama.PNG')) {
+        displayImage.src = "./img/caca.PNG"
+    } else {
+        displayImage.src = "./img/tama.PNG"
+    }
+}
+
+function death() {
+    let displayImage = document.getElementById('content')
+    if (displayImage.src.match('./img/tama.PNG')) {
+        displayImage.src = "./img/muerte.PNG"
+    } else {
+        displayImage.src = "./img/huevo.PNG"
+    }
 }
 
 
-console.log ('Hola carcola');
