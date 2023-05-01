aba4dae6f1550ad7cd1bbda24f46e294191b6670
DitGrau <juditgraup@gmail.com>
2023-05-01 16:41:54 +0200

    Vuelve a no funcionar nada, incluso quitando el JS, no entiendo. Dejo la versión que funciona en el VSC aunque no se visualice en PAGES

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@
 
 function birth() {
     let displayImage = document.getElementById('content')
-    if (displayImage.src.match('../Tamagotchi/img/huevo.png')) {
-        displayImage.src = "../Tamagotchi/img/tama.png"
+    if (displayImage.src.match('./img/huevo.png')) {
+        displayImage.src = "./img/tama.png"
     }
 }
 
 function popo() {
     let displayImage = document.getElementById('content')
-    if (displayImage.src.match('../Tamagotchi/img/tama.png')) {
-        displayImage.src = "../Tamagotchi/img/caca.png"
+    if (displayImage.src.match('./img/tama.png')) {
+        displayImage.src = "./img/caca.png"
     } else {
-        displayImage.src = "../Tamagotchi/img/tama.png"
+        displayImage.src = "./img/tama.png"
     }
 }
 
 function death() {
     let displayImage = document.getElementById('content')
-    if (displayImage.src.match('../Tamagotchi/img/caca.png')) {
-        displayImage.src = "../Tamagotchi/img/muerte.png"
+    if (displayImage.src.match('./img/caca.png')) {
+        displayImage.src = "./img/muerte.png"
     } else {
-        displayImage.src = "../Tamagotchi/img/huevo.png"
+        displayImage.src = "./img/huevo.png"
     }
 }
